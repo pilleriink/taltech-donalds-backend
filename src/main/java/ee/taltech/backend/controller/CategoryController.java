8e5236e47ede35894d6ee415694c95466df153e6
pikoiv <pikoiv@ttu.ee>
2020-09-21 09:36:42 +0300

    add CategoryController and CategoryService

---
+++ src/main/java/ee/taltech/backend/controller/CategoryController.java
@@ -0,0 +1,45 @@
+package ee.taltech.backend.controller;
+
+import ee.taltech.backend.exception.InvalidProductException;
+import ee.taltech.backend.exception.ProductNotFoundException;
+import ee.taltech.backend.model.Category;
+import ee.taltech.backend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("categories")
+@RestController
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+
+    @GetMapping
+    public List<Category> findAll(@PathVariable Long id) throws ProductNotFoundException {
+        return categoryService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Category getCategoryById(@PathVariable Long id) throws ProductNotFoundException {
+        return categoryService.findById(id);
+    }
+
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category) throws InvalidProductException {
+        return categoryService.save(category);
+    }
+
+    @PutMapping("{id}")
+    public Category updateProduct(@RequestBody Category category, @PathVariable Long id) throws InvalidProductException, ProductNotFoundException {
+        return categoryService.update(category, id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCategory(@PathVariable Long id) throws ProductNotFoundException {
+        categoryService.delete(id);
+    }
+
+}
---
+++ src/main/java/ee/taltech/backend/service/CategoryService.java
@@ -0,0 +1,50 @@
+package ee.taltech.backend.service;
+
+import ee.taltech.backend.exception.InvalidProductException;
+import ee.taltech.backend.exception.ProductNotFoundException;
+import ee.taltech.backend.model.Category;
+import ee.taltech.backend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category save(Category category) throws InvalidProductException {
+        if (category.getName() == null) {
+            throw new InvalidProductException("Invalid product");
+        }
+        if (category.getId() != null) {
+            throw new InvalidProductException("Product already exists");
+        }
+        return categoryRepository.save(category);
+    }
+
+    public Category findById(Long id) throws ProductNotFoundException {
+        return categoryRepository.findById(id)
+                .orElseThrow(ProductNotFoundException::new);
+    }
+
+    public Category update(Category category, Long id) throws InvalidProductException, ProductNotFoundException {
+        if (category.getName() == null) {
+            throw new InvalidProductException("Invalid product");
+        }
+        Category databaseCategory = findById(id);
+        databaseCategory.setName(category.getName());
+        return categoryRepository.save(databaseCategory);
+    }
+
+    public void delete(Long id) throws ProductNotFoundException {
+        Category databaseCategory = findById(id);
+        categoryRepository.delete(databaseCategory);
+    }
+}
