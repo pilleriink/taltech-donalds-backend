af2feed7b948d9a7aa93a93d6dde55ad2137d3ba
sopham <shuong1012@gmail.com>
2020-09-24 11:51:24 +0300

    add location

---
+++ src/main/java/ee/taltech/backend/model/location/Location.java
@@ -0,0 +1,30 @@
+package ee.taltech.backend.model.location;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Location {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String address;
+    private Double lon;
+    private Double lat;
+
+    public Location(String name, String address, Double lon, Double lat) {
+        this.name = name;
+        this.address = address;
+        this.lon = lon;
+        this.lat = lat;
+    }
+}
