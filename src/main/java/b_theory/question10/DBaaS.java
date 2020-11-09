480b973ed175e08486cd0582974f76dd0a6e912b
pikoiv <pikoiv@ttu.ee>
2020-11-09 09:40:02 +0200

    added b_theory answers

---
+++ src/main/java/b_theory/question10/DBaaS.java
@@ -0,0 +1,33 @@
+package b_theory.question10;
+
+public class DBaaS {
+
+    //todo A
+    // What is DBaaS (Database as a service)?
+    // Answer: DBaaS provides users access to a database without the need for setting up physical software
+
+    //todo B
+    // Name 2 examples of DBaaS.
+    // 1 Oracle Database
+    // 2 Amazon relational Database Service
+
+    //todo C
+    // Name and explain 2 benefits of using DBaaS
+    // 1 DBaaS minimizes cost, because it gives you estimated costs for the specific type of appliance you are going to select
+    // 2 Security, kernel and OS updates are done by the provider as it's a fully-managed DB service
+
+    //todo D
+    // Name and explain 1 drawback of using DBaaS
+    // 1 Security and data privacy, because you don't have control to limit people who gain access
+
+    //todo E
+    // What other -aaS do you know?
+    // 1 SaaS - Software as a Service
+    // 2 PaaS - Platform as a Service
+    // 3 IaaS - Infrastructure as a Service
+
+    //todo F
+    // In your opinion which aaS are the most useful and should be integrated first.
+    // Answer:
+
+}
