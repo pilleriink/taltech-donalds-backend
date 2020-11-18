2750fb94e6b20a822001ab86198275efed6ae677
sopham <sopham@ttu.ee>
2020-11-18 17:36:26 +0200

    user model

---
+++ src/main/java/ee/taltech/backend/model/user/User.java
@@ -0,0 +1,18 @@
+package ee.taltech.backend.model.user;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+}
