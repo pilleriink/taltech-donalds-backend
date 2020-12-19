ead560f7b2fbed1f73b3942689bfc2d92de5fdba
sopham <sopham@ttu.ee>
2020-12-19 20:56:51 +0200

    theory: phones

--- src/main/java/c_theory/question14/phones/App.java
+++ src/main/java/c_theory/question14/phones/App.java
@@ -2,9 +2,9 @@ package c_theory.question14.phones;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@Entity
+//@Getter
+//@Setter
+//@Entity
 public class App {
     private String name;
     private String author;
